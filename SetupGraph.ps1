6f3527b0ca30753964ee196c6d0c5ce3e4b2acf5
David E. Lambert <david@amberanalytics.net>
2023-11-30 13:02:17 -0500

    parameterize for agnosticity; revise Graph scopes

--- SetupGraph.ps1
+++ SetupGraph.ps1
@@ -1,16 +1,22 @@
-$automationAccountName = "OnboardingAutomation"
+param (
+    [Parameter(Mandatory = $true, Position = 0)]
+    [string]$AutomationAccountName
+)
+
 $graphScopes = @(
     "Directory.ReadWrite.All",
     "Group.ReadWrite.All",
-    "Organization.Read.All",
+    "Mail.ReadWrite",
     "Mail.Send",
+    "Organization.Read.All",
+    "Sites.Manage.All",
     "User.ReadWrite.All",
     "UserAuthenticationMethod.ReadWrite.All"
 )
 
 # Connect as an Administrator, NOT the Automation account
 Connect-MgGraph -Scopes "Application.Read.All", "AppRoleAssignment.ReadWrite.All,RoleManagement.ReadWrite.Directory" -NoWelcome
-$managedIdentityId = (Get-MgServicePrincipal -Filter "displayName eq '$automationAccountName'").Id
+$managedIdentityId = (Get-MgServicePrincipal -Filter "displayName eq '$AutomationAccountName'").Id
 $graphApp = Get-MgServicePrincipal -Filter "AppId eq '00000003-0000-0000-c000-000000000000'" # always this GUID
 foreach ($scope in $graphScopes) {
     $appRole = $graphApp.AppRoles | Where-Object { $_.Value -eq $scope }
