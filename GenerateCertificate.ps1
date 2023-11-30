4b35123f6326d5028daeb90b5f1a4f6141216d9f
David E. Lambert <david@amberanalytics.net>
2023-11-30 13:01:12 -0500

    Parameterize for client-agnostic use

--- GenerateCertificate.ps1
+++ GenerateCertificate.ps1
@@ -1,8 +1,28 @@
-$Cert = New-SelfSignedCertificate -FriendlyName "Wynnefield Onboarding Certificate" `
-    -Subject "Graph certificate for Wynnefield Onboarding App" `
-    -CertStoreLocation Cert:\CurrentUser\My\ `
-    -NotAfter (Get-Date).AddYears(1)
-$PathPrefix = "C:\Users\DavidLambert\OneDrive - Integrid LLC\onboard\Wynnefield-Onboarding-Expires-" `
-    + (Get-Date).AddYears(1).ToString("yyyy-MM-dd")
-Export-Certificate -Cert $Cert -FilePath "$PathPrefix.cer"
-Export-PfxCertificate -Cert $Cert -FilePath "$PathPrefix.pfx" -Password (Get-Credential).password
+param (
+    [Parameter(Mandatory = $true)]
+    [ValidateScript({
+            if (-Not ($_ | Test-Path) ) {
+                throw "Directory does not exist"
+            }
+            if (-Not ($_ | Test-Path -PathType Container) ) {
+                throw "The -Directory argument point to a directory (folder). File paths are not allowed."
+            }
+            return $true
+        })]
+    [System.IO.FileInfo]$Directory = (Get-Location).Path,
+
+    [Parameter(Mandatory = $true)]
+    [int]$DaysValid = 365,
+
+    [Parameter(Mandatory = $true)]
+    [string]$FriendlyName,
+
+    [Parameter(Mandatory = $true)]
+    [string]$Subject
+)
+
+$endDate = (Get-Date).AddDays($DaysValid)
+$pathPrefix = "$Directory\$FriendlyName - Expires$($endDate.ToString("yyyy-MM-dd"))"
+$cert = New-SelfSignedCertificate -FriendlyName $FriendlyName -Subject $Subject -CertStoreLocation Cert:\CurrentUser\My\ -NotAfter $endDate
+Export-Certificate -Cert $cert -FilePath "$pathPrefix.cer"
+Export-PfxCertificate -Cert $cert -FilePath "$pathPrefix.pfx" -Password (Get-Credential).password
